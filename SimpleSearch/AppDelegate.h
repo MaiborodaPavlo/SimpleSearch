8e4b105600ed3825470d947ce351ecba205b568d
Pavel Mayboroda <Successbloger1@gmail.com>
2018-01-26 14:24:57 +0200

    Initial Commit

---
+++ SimpleSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleSearch
+//
+//  Created by Pavel on 26.01.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleSearch
+//
+//  Created by Pavel on 26.01.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
